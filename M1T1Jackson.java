a28bdf52869aa2069ed4dc3a56f9f5db9a22ee57
Jacksona6180 <113918091+Jacksona6180@users.noreply.github.com>
2023-08-23 12:36:13 -0400

    Add files via upload
    M1T1- Orientation Project

---
+++ M1T1Jackson.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println(" CTS 285\n" +
+        "// M1T1\n" +
+        "// Jackson, Ashley \n" +
+        "// August 23, 2023 \n" +
+        "// I am currently studying Java, Linux, and Python this semester."
+        + " I am a gym enthusiast and social media health advocate!");
+  }
+}
